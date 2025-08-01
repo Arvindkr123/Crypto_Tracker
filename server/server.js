c4fdff16e0b2b0b4e4c00e89e67483f58e71ebf8
= <=>
2025-08-01 23:32:46 +0530

    added change package json file

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import config from "./src/config/config.js";
+import dotenvData from "./src/config/config.js";
+import { startCron } from "./src/corn-jobs/syncPrices.js";
+import router from "./src/routes/coins.routes.js";
+import connectToDBHandler from "./src/utils/Connection.js";
+import express from "express"
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: config.CLIENT_WEBAPP_URL,
+  credentials: true
+}));
+
+app.use("/welcome", (req, res) => {
+  res.send(200).json({
+    success: true,
+    message: 'Welcome to Crypto Tracker App'
+  })
+})
+
+app.use("/api", router)
+
+connectToDBHandler()
+  .then(() => {
+    app.listen(dotenvData.PORT, () => {
+      console.log('✅ Server running on port', dotenvData.PORT);
+      startCron();
+    });
+  })
+  .catch((err) => {
+    console.error('❌ DB connection failed:', err);
+  });
